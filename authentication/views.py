28b0b45c7aa28159dbb863811e6c2cd7ad5eb699
dienzahraaa <dienzahra312@gmail.com>
2024-05-02 09:51:17 +0700

    Mengerjakan autentikasi dan navbar

--- authentication/views.py
+++ authentication/views.py
@@ -1,31 +1,15 @@
 from django.shortcuts import render
 from django.shortcuts import redirect
-from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages  
 from django.contrib.auth import authenticate, login, logout
-
-# Create your views here.
-# def register(request):
-#     form = UserCreationForm()
-
-#     if request.method == "POST":
-#         form = UserCreationForm(request.POST)
-#         if form.is_valid():
-#             form.save()
-#             messages.success(request, 'Your account has been successfully created!')
-#             return redirect('main:login')
-#     context = {'form':form}
-#     return render(request, 'register.html', context)
-
 from django.shortcuts import render, redirect
 from django.contrib import messages
-from .forms import CustomUserCreationForm  # Impor formulir kustom
+from .forms import SimpleUserCreationForm 
 
 def register(request):
-    form = CustomUserCreationForm()  # Gunakan formulir kustom
-
+    form = SimpleUserCreationForm() 
     if request.method == "POST":
-        form = CustomUserCreationForm(request.POST)
+        form = SimpleUserCreationForm(request.POST)
         if form.is_valid():
             form.save()
             messages.success(request, 'Your account has been successfully created!')
@@ -53,6 +37,5 @@ def logout_user(request):
     return redirect('main:login')
 
 def show_main(request):
-    context = {    }
-
+    context = {}
     return render(request, "main.html", context)
